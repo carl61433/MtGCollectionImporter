ecc1367b8b70652dfc229b4b34210a2c805e31d1
carl61433 <cd.kopf@gmail.com>
2025-07-27 14:42:15 +0000

    Now recieving data from the webpage, need to add more to work with the data next.

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type card struct {
-	set  string
-	num  string
-	name string
-	foil bool
+type Card struct {
+	set   string `json:"code"`
+	num   string `json:"num"`
+	name  string `json:"name"`
+	foil  string `json:"foil"`
+	count int    `json:"count"`
 }
 
-func newCard(set string, num string) *card {
-	c := card{}
+func newCard(set string, num string, foil string) *Card {
+	c := Card{}
 	c.set = set
 	c.num = num
+	c.foil = foil
+	c.count = 0
 
 	//path := 1
-	//fmt.Println("MtG Set: ", set, "Card number: ", num)
+	fmt.Println("MtG Set: ", c.set, "Card number: ", c.num)
 	return (&c)
 }
 
 func main() {
+
 	fmt.Println("Listening...")
 	http.HandleFunc("/submit", cardRequest)
 	http.Handle("/", http.FileServer(http.Dir(".")))
@@ -30,8 +35,15 @@ func main() {
 }
 
 func cardRequest(rw http.ResponseWriter, r *http.Request) {
-	fmt.Println("test")
-	setCode := r.FormValue("set-code")
-	cardNo := r.FormValue("card-number")
-	newCard(setCode, cardNo)
+	var card Card
+
+	// decode input or return error
+	err := json.NewDecoder(r.Body).Decode(&card)
+	if err != nil {
+		rw.WriteHeader(400)
+		fmt.Fprintf(rw, "Decode error! please check your JSON formating.")
+		return
+	}
+	fmt.Println(card.set, card.num, card.foil)
+	newCard(card.set, card.num, card.foil)
 }
