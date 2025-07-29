b5d79992268a09c8b8f5705ee81cc687143155a9
carl61433 <cd.kopf@gmail.com>
2025-07-29 16:02:17 +0000

    Am able to grab the card JSON from Scryfall now. Next is working with it, and incrementing count when the same card is added

--- main.go
+++ main.go
@@ -4,30 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type Card struct {
-	set   string `json:"code"`
-	num   string `json:"num"`
-	name  string `json:"name"`
-	foil  string `json:"foil"`
-	count int    `json:"count"`
-}
-
-func newCard(set string, num string, foil string) *Card {
-	c := Card{}
-	c.set = set
-	c.num = num
-	c.foil = foil
-	c.count = 0
-
-	//path := 1
-	fmt.Println("MtG Set: ", c.set, "Card number: ", c.num)
-	return (&c)
+	Set   string `json:"set"`
+	Num   string `json:"num"`
+	Foil  bool   `json:"foil"`
+	Name  string `json:"name"`
+	Count int
 }
 
 func main() {
-
 	fmt.Println("Listening...")
 	http.HandleFunc("/submit", cardRequest)
 	http.Handle("/", http.FileServer(http.Dir(".")))
@@ -36,14 +25,54 @@ func main() {
 
 func cardRequest(rw http.ResponseWriter, r *http.Request) {
 	var card Card
-
 	// decode input or return error
 	err := json.NewDecoder(r.Body).Decode(&card)
 	if err != nil {
 		rw.WriteHeader(400)
-		fmt.Fprintf(rw, "Decode error! please check your JSON formating.")
+		fmt.Fprintf(rw, "Decode error! please check your JSON formating.\n")
 		return
 	}
-	fmt.Println(card.set, card.num, card.foil)
-	newCard(card.set, card.num, card.foil)
+	//fmt.Printf("Output: " + card.Set + "," + card.Num + "," + card.Foil)
+	//fmt.Println(card) //Example: {FIN 1 false  0}
+	url := "https://api.scryfall.com/cards/" + card.Set + "/" + card.Num
+	getCardName(url)
+}
+
+func getCardName(url string) *Card {
+	c := Card{}
+	// Create a new HTTP client
+	client := &http.Client{
+		Timeout: time.Second * 10, // Timeout each requests
+	}
+	method := "GET"
+	payload := strings.NewReader(`{"key1":"value1", "key2":"value2"}`)
+
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return &c
+	}
+	// Set headers
+	req.Header.Add("Content-Type", "application/json")
+
+	// Execute the request using the custom HTTP client
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return &c
+	}
+	defer resp.Body.Close()
+
+	// Read and print response
+	err = json.NewDecoder(resp.Body).Decode(&c)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return &c
+	}
+	fmt.Println(c)
+	c.Count += 1
+
+	//path := 1
+	//fmt.Println("MtG Set: ", c.Set, "Card number: ", c.Num)
+	return &c
 }
